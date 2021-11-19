7090e6249ff87f1903f9f30607a58d748c7a72fb
idioqa <polidiosyn@gmail.com>
2021-11-19 16:58:05 +0200

    Added tasks to 26 to lists

--- lists/lists.py
+++ lists/lists.py
@@ -82,5 +82,150 @@ def sum_list(arr):
     sum_arr.append(sum)
   return sum_arr
 
-print(sum_list(separate(whole_nums, 5)))
-      
+print("sum_list = ", sum_list(separate(whole_nums, 5)))
+
+#13. Написать функцию которая на вход получает список 5_stars, а вернёт 2 списка. 
+# В одном списке внутренние списки из 5_stars сумма чисел которых >= 100, а другой сумма чисел которых < 100. 
+# Если какого-то списка не получится, то вместо него вернуть текст “No lists”
+
+def two_sum_list(arr):
+  sum_arr_less = []
+  sum_arr_more = []
+  temp_arr = []
+  for i in arr:
+    sum = 0
+    for j in range(len(i)):
+      sum += i[j]
+      temp_arr.append(i[j])
+    if(sum >= 100):
+      sum_arr_more.append(temp_arr)
+    elif( sum < 100):
+      sum_arr_less.append(temp_arr)
+    temp_arr = []
+  if(len(sum_arr_less) == 0): 
+    sum_arr_less == "No lists"
+  if(len(sum_arr_more) == 0): 
+    sum_arr_more == "No lists"
+  return sum_arr_less, sum_arr_more
+
+print("Two lists with >= 100 and < 100", two_sum_list(separate(whole_nums, 5)))
+
+#16. Написать скрипт который сгенерирует список имён пользователей. Список имён вывести в консоль.
+#17. Написать скрипт который сгенерирует список имён файлов. К каждому имени  
+# файла надо прикрепить номер итерации цикла как порядковый номер. 
+
+def create_users(pattern, n):
+  users = [pattern + str(i) for i in range(n)]
+  return users
+
+print("Users: ", create_users("User", 100))
+
+#18. Написать скрипт который сгенерирует список списков. Каждый элемент списка это список в котором 
+# 0-й элемент - это имя пользователя, а 1-й - элемент это дата регистрации.
+
+from random import randrange
+from datetime import timedelta
+
+def random_date(start, end):
+    delta = end - start
+    int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+    random_second = randrange(int_delta)
+    return start + timedelta(seconds=random_second)
+
+from datetime import datetime
+d1 = datetime.strptime('1/1/1993 1:30 PM', '%m/%d/%Y %I:%M %p')
+d2 = datetime.strptime('1/1/2021 4:50 AM', '%m/%d/%Y %I:%M %p')
+
+def create_users_list(pattern, n): return [[pattern + str(i), str(random_date(d1, d2))] for i in range(n)]
+
+my_users = create_users_list("login", 100)
+print(my_users)
+
+#19. Написать скрипт который сгенерирует список Employees списков . Каждый элемент списка - это список в котором:
+#0-й - элемент - это имя пользователя,
+#1-й - элемент - это логин,
+#2-й - элемент - это пароль,
+#3-й - элемент - это email (email тоже генерировать),
+#4-й - элемент - это дата регистрации
+
+import secrets
+import string
+
+def get_password():
+  return ''.join(secrets.choice(string.ascii_letters + string.digits) for i in range(20))
+
+def get_email(name, login):
+  return name + login + "@" + "gmail.com"
+
+def create_employees_list(n): 
+  return [
+    [
+      "name" + str(i),
+      "login" + str(i), 
+      str(datetime.now()), 
+      get_email("Name" + str(i), "Login" + str(i)),
+      get_password()
+      ] 
+    for i in range(n)]
+
+print(create_employees_list(100))
+
+#20.Написать скрипт который сгенерирует список family списков. Каждый элемент списка - это список в котором:
+#0-й - элемент - это логин,
+#1-й - элемент - это имя,
+#2-й - элемент - семейный статус (True, False - генерировать рандомно)
+
+def generate_family_list(n):
+  return [[
+    "login" + str(i), 
+    "name" + str(i),
+    random.choice([True, False])
+    ] for i in range(n)]
+  
+print("\nFamily: ", generate_family_list(10))
+
+#21. Написать скрипт который сгенерирует список gender списков. Каждый элемент списка - это список в котором:
+#0-й - элемент - это логин,
+#1-й - элемент - это имя,
+#2-й - элемент - гендер (1-м, 0-ж)
+
+def get_genders(n):
+  return [[
+      "login" + str(i), 
+      "name" + str(i),
+      random.randint(0, 1)
+    ] for i in range(n)]
+  
+print("\nGenders: ", get_genders(10))
+
+#22. Написать скрипт который сгенерирует список salary списков. Каждый элемент списка - это список в котором:
+#0-й - элемент - это логин,
+#1-й - элемент - это имя,
+#2-й - элемент - зарплата (генерироовать от 300$ до 5000$)
+
+def get_salaries(n):
+  return [[
+      "login" + str(i), 
+      "name" + str(i),
+      str(random.randint(300, 5000))
+  ] for i in range(n)]
+  
+print("\nSalaries: ", get_salaries(10))
+
+#23. Написать скрипт который создаст список имён работников из salary у которых ЗП от 1500$ до 3000$
+avg_salaries = []
+for item in get_salaries(100):
+  if int(item[2]) > 1500 and int(item[2]) > 3000: avg_salaries.append(item[0])
+  
+print("avg_salaries: ", avg_salaries)
+
+#24.Написать скрипт который создаст список имён мужчин из gender.
+#25.Написать скрипт который создаст список имён женщин из gender.
+man = [i[0] for i in get_genders(100) if i[2] == 0]
+print("\nMan: ", man)
+
+woman = [i[0] for i in get_genders(100) if i[2] == 1]
+
+#26.Написать скрипт который создаст список имён неженатых мужчин из family.
+#27.Написать скрипт который создаст список имён незамужних женщин из family.
+
